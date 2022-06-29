7afe25a3dbeb89cecb7f2d8d3d144e7acb93a785
jaswindersingh2601 <101857182+jaswindersingh2601@users.noreply.github.com>
2022-06-29 22:38:01 +0530

    star rating is add.

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ db.once("open", () => {
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended : "true"}))
 app.use(methodOverride('_method'))
 
